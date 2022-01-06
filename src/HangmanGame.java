2ae5df011edee74c8747d1052312bf72854e18f3
wadilari <90609440+wadilari@users.noreply.github.com>
2022-01-06 21:29:00 +0100

    method structure

--- src/HangmanGame.java
+++ src/HangmanGame.java
@@ -7,22 +7,29 @@ public class HangmanGame {
         HangmanGame game = new HangmanGame();
         game.run();
 
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("Willkommen bei Hangman!");
-//        System.out.println("Bitte gebe ein Wort ein:");
-//        String wort = sc.next();
-//        System.out.println("");
-//        System.out.println("----------------------------------");
-//        System.out.println("");
-//        System.out.println("Bitte gebe einen Buchstaben ein:");
-//        String guess = sc.next();
-//        if (guess
     }
 
     /**
-     * Startet ein Spiel, fragt am Ende, ob es noch eins geben soll und startet gegebenenfalls ein weiteres.
+     * Hauptmethode, startet das Spiel und beendet es, wenn es keine weitere Runde geben soll.
      */
     public void run() {
 
     }
+
+    /**
+     *
+     */
+    public void round() {
+        return;
+    }
+
+    public static String insertWord() {
+        return "";
+    }
+
+    public static char insertGuess() {
+        return ' ';
+    }
+
+    public
 }
