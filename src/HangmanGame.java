d4afb75f67d91c1f519aa79b4812bcc74f091e00
wadilari <90609440+wadilari@users.noreply.github.com>
2022-01-06 13:06:16 +0100

    Initial commit

---
+++ src/HangmanGame.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class HangmanGame {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Willkommen bei Hangman!");
+        System.out.println("Bitte gebe ein Wort ein:");
+        String wort = sc.next();
+        System.out.println("");
+        System.out.println("----------------------------------");
+        System.out.println("");
+        System.out.println("Bitte gebe einen Buchstaben ein:");
+        String guess = sc.next();
+        if (guess
+    }
+}
